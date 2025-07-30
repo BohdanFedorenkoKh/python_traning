c537b27d875ef6569a0f364965630923cdb9caba
Bohdan Fedorenko <bohdanfedorenko@icloud.com>
2025-07-30 17:37:13 +0300

    New file format

---
+++ format.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+age = 26
+name = 'Bohdan'
+
+print('Возраст {0}--{1}'.format(name,age))
+pritn('Почему {0}-- забавляется с этим Python'.format(name))
--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,2 @@
-print('Hello world!')
+#!/usr/bin/env python3
+print('Hello world!') # ptint это функция 
