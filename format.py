76ebee842406364579c01a280457606ffc9544b2
Bohdan Fedorenko <bohdanfedorenko@icloud.com>
2025-07-30 17:41:43 +0300

    file format change

--- format.py
+++ format.py
@@ -3,4 +3,4 @@ age = 26
 name = 'Bohdan'
 
 print('Возраст {0}--{1}'.format(name,age))
-pritn('Почему {0}-- забавляется с этим Python'.format(name))
+print('Почему {0}-- забавляется с этим Python'.format(name))
